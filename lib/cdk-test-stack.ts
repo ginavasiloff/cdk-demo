3b8eaaa5eb44537d5864698ae121503423742d86
gina-vasiloff <gina-vasiloff@pluralsight.com>
2023-05-26 09:28:03 -0600

    Add hello world lambda and endpoint

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { HelloWorld } from '../lambda/hello';
+
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    new HelloWorld(this, 'hello-world');
 
   }
 }
